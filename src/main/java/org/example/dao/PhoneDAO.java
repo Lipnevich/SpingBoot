ac80924c7893efec823d40d1c515131f1c9f3fb1
Nicolay Lipnevich <n.lipnevich@intechcore.com>
2023-02-08 20:11:08 +0300

    Init

--- src/main/java/org/example/dao/PhoneDAO.java
+++
@@ -1,9 +0,0 @@
-package org.example.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PhoneDAO extends CrudRepository<PhoneEntity, Long> {
-
-}
